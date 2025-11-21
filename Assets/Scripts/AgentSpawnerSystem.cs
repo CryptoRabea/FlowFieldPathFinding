ce37a64ead8d94adfa39832137f5594ff19ef053
youssef mostafa rabea <Youssefrabea3@gmail.com>
2025-11-21 06:45:09 +0200

    .

--- Assets/Scripts/AgentSpawnerSystem.cs
+++ Assets/Scripts/AgentSpawnerSystem.cs
@@ -2,10 +2,10 @@ using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.Rendering;
+
 
 namespace FlowFieldPathfinding
 {
